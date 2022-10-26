c013f31630e8d9566eecd61015f7f491a8bb33a1
Malte Koch <malte-koch@gmx.net>
2022-10-26 14:05:10 +0200

    update save

--- python/examples/save.py
+++ python/examples/save.py
@@ -1,3 +1,4 @@
+import gzip
 import os
 import traceback
 import ydlidar
@@ -9,6 +10,8 @@ import matplotlib.animation as animation
 import numpy as np
 import logging
 from timeit import default_timer
+from pathlib import Path
+import time
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -32,28 +35,45 @@ laser.setlidaropt(ydlidar.LidarPropSerialBaudrate, 512000)
 laser.setlidaropt(ydlidar.LidarPropLidarType, ydlidar.TYPE_TOF)  # TYPE_LIDAR
 laser.setlidaropt(ydlidar.LidarPropDeviceType, ydlidar.YDLIDAR_TYPE_SERIAL)
 laser.setlidaropt(ydlidar.LidarPropScanFrequency, 10.0)
-laser.setlidaropt(ydlidar.LidarPropSampleRate, 20)
+laser.setlidaropt(ydlidar.LidarPropSampleRate, 10)
 laser.setlidaropt(ydlidar.LidarPropSingleChannel, False)
 scan = ydlidar.LaserScan()
 
+# angle increment: 0.0028508100658655167
+
 ret = laser.initialize()
+output_file = Path("scans", f"lidar_scan_{time.time()}.csv.gz")
+output_file.parent.mkdir(exist_ok=True, parents=True)
 
 try:
     if ret:
         ret = laser.turnOn()
         scan = ydlidar.LaserScan()
         prev = default_timer()
+        with gzip.open(output_file, "wb") as writer:
+            writer.write(b"ts;points;inc\n")
 
-        while ret and ydlidar.os_isOk():
-            r = laser.doProcessSimple(scan)
-            current = default_timer()
-            if r:
-                logging.info(
-                    f"scan received[{scan.stamp} - elapsed={(current - prev):.3f}s] {scan.points.size()} ranges is [{1.0/scan.config.scan_time}] Hz")
-            else:
-                logging.warning("Failed to get Lidar Data.")
+            while ret and ydlidar.os_isOk():
+                r = laser.doProcessSimple(scan)
+                current = default_timer()
+              
+                if r:
+                    points = []
+                    for p in scan.points:
+                        intensity = p.intensity
+                        angle = p.angle
+                        rangee = p.range
+                        points.append(rangee)
 
-            prev = current
+                    points_str = ",".join(map(str, points))
+                    writer.write(bytes(f"{scan.stamp};{points_str};{scan.config.angle_increment}\n", 'utf-8'))
+
+                    logging.info(
+                        f"scan received[{scan.stamp} - elapsed={(current - prev):.3f}s] {scan.points.size()} ranges is [{1.0/scan.config.scan_time}] Hz")
+                else:
+                    logging.warning("Failed to get Lidar Data.")
+
+                prev = current
         laser.turnOff()
 except Exception as e:
     traceback.print_exc()
